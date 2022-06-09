46a86bc5eabb17eb6747893c3c9a1d596634b68c
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-06-08 21:16:22 -0500

    :art: cancel room search previous fetch on autocomplete component

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -1,7 +1,7 @@
 import { CloseIcon, SearchIcon } from "@chakra-ui/icons"
-import { ModalOverlay, Modal, ModalContent, ModalBody, Box, VStack, HStack, Input, Text, InputLeftElement, InputGroup, IconButton, Divider, Badge, StackDivider } from "@chakra-ui/react"
-import { BuildingList, RoomRetrieve, RoomsService } from "api_clients"
-import { FC, useState } from "react"
+import { ModalOverlay, Modal, ModalContent, ModalBody, Box, VStack, HStack, Input, Text, InputLeftElement, InputGroup, IconButton, Divider, Badge, StackDivider, Spinner } from "@chakra-ui/react"
+import { BuildingList, CancelablePromise, RoomRetrieve, RoomsService } from "api_clients"
+import { FC, useEffect, useState } from "react"
 
 
 interface SearchModalProps {
@@ -12,25 +12,41 @@ interface SearchModalProps {
 
 
 const BuildingsAndRoomsSearchModal: FC<SearchModalProps> = ({buildings, onClose, isOpen}: SearchModalProps) => {
+    const [search, setSearch] = useState<string>("")
     const [isLoadingRooms, setIsLoadingRooms] = useState(false)
     const [filteredRooms, setFilteredRooms] = useState<RoomRetrieve[]>([])
     const [filteredBuildings, setFilteredBuildings] = useState<BuildingList[]>([])
 
-    const searchRooms = (search: string) => {
-        if (search.length == 0)
-            return setFilteredRooms([])
+    useEffect(() => {
+        if (search.length == 0) {
+            setIsLoadingRooms(false)
+            setFilteredRooms([])
+            return
+        }
 
         setIsLoadingRooms(true)
 
-        RoomsService.roomsListStandalone(search)
-            .then((rooms: RoomRetrieve[]) => {
+        let roomFetchPromise: CancelablePromise<RoomRetrieve[]>|null = null
+        const timeoutCall = setTimeout(() => {
+            roomFetchPromise = RoomsService.roomsListStandalone(search)
+            roomFetchPromise.then((rooms: RoomRetrieve[]) => {
                 setFilteredRooms(rooms)
                 setIsLoadingRooms(false)
             })
-            .catch((error: any) => console.error("Search", error))
-    }
+            .catch((e: any) => {
+                console.error("Rooms search", e)
+                setIsLoadingRooms(false)
+            })
+        }, 900)
 
-    const searchBuildings = (search: string) => {
+        return () => {
+            clearTimeout(timeoutCall)
+            if (roomFetchPromise)
+                roomFetchPromise.cancel()
+        }
+    }, [search])
+
+    useEffect(() => {
         if (search.length == 0)
             return setFilteredBuildings([])
 
@@ -42,15 +58,7 @@ const BuildingsAndRoomsSearchModal: FC<SearchModalProps> = ({buildings, onClose,
         )
 
         setFilteredBuildings(filtered)
-    }
-
-
-    const onInputChange = (e: any) => {
-        let value = e.target.value
-
-        searchBuildings(value)
-        searchRooms(value)
-    }
+    }, [search])
 
     return (
         <Modal onClose={onClose} isOpen={isOpen} size="xl">
@@ -63,7 +71,8 @@ const BuildingsAndRoomsSearchModal: FC<SearchModalProps> = ({buildings, onClose,
                             <InputGroup>
                                 <InputLeftElement children={<SearchIcon />}/>
                                 <Input 
-                                    onChange={onInputChange}
+                                    value={search}
+                                    onChange={(e) => setSearch(e.target.value)}
                                     rounded="full" autoFocus variant="filled"/>
                             </InputGroup>
 
@@ -99,12 +108,17 @@ const BuildingsAndRoomsSearchModal: FC<SearchModalProps> = ({buildings, onClose,
                         }
 
                         {/* Rooms */}
+                        {filteredRooms.length == 0 && isLoadingRooms && <Text color="gray" py={5}>Cargando salones...</Text>}
                         {
                             filteredRooms.length > 0 && (
                                 <VStack align="start" width="100%">
                                     <Divider borderColor="gray.300" />
 
-                                    <Text fontSize="lg" pb={1} fontWeight="bold">Salones</Text>
+                                    <HStack width="100%" justifyContent="space-between">
+                                        <Text fontSize="lg" pb={1} fontWeight="bold">Salones</Text>
+
+                                        {isLoadingRooms && <Spinner />}
+                                    </HStack>
 
                                     <VStack width="100%" px={2} align="start" spacing={2.5} divider={<StackDivider />}>
                                         {
