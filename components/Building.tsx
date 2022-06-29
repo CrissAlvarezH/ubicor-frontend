85e8e5fc00f1d409fa5e47a9f361abe1b1fc348e
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-06-29 00:11:29 -0500

    :bug: show building images on grid and on map marker

--- components/Building.tsx
+++ components/Building.tsx
@@ -1,16 +1,28 @@
 import { FC } from "react"
 import { Badge, Box, Grid, GridItem, Heading, Text } from "@chakra-ui/react"
+import { OpenAPI } from "api_clients"
 import Image from "next/image"
 
-import { BuildingList } from "api_clients"
+import { BuildingImageRetrieve, BuildingList } from "api_clients"
 import { zoneColorSchemas } from "utils/styles"
+
+
+export function getImageUrl(buildingImages: BuildingImageRetrieve[], position: number): string {
+    let path = "/empty-img.png" // by default each image is empty
+    if (buildingImages.length > position)
+        if (position === 0)
+            path = OpenAPI.BASE + buildingImages[position].image.medium
+        else 
+            path = OpenAPI.BASE + buildingImages[position].image.small
+
+    return path
+}
 
 interface BuildingProps {
     building: BuildingList
 }
 
-const Building: FC<BuildingProps> = ({building}) => {
-
+const Building: FC<BuildingProps> = ({building}: BuildingProps) => {
 
     return (
         <Box shadow="md" background="white" borderWidth="1px" h="full" rounded="md" cursor="pointer">
@@ -24,15 +36,15 @@ const Building: FC<BuildingProps> = ({building}) => {
                 overflow="hidden"
                 >
                 <GridItem rowSpan={2} colSpan={4} bg='tomato' position="relative">
-                    <Image src="https://picsum.photos/300/100" layout="fill" objectFit="cover" alt="Building"/>
+                    <Image src={getImageUrl(building.building_images, 0)} layout="fill" objectFit="cover" alt="Building"/>
                 </GridItem>
 
                 <GridItem rowSpan={1} colSpan={2} bg='papayawhip' position="relative">
-                    <Image src="https://picsum.photos/450/100" layout="fill" objectFit="cover" alt="Building"/>
+                    <Image src={getImageUrl(building.building_images, 1)} layout="fill" objectFit="cover" alt="Building"/>
                 </GridItem>
 
                 <GridItem rowSpan={1} colSpan={2} bg='tomato' position="relative">
-                    <Image src="https://picsum.photos/400/100" layout="fill" objectFit="cover" alt="Building"/>
+                    <Image src={getImageUrl(building.building_images, 2)} layout="fill" objectFit="cover" alt="Building"/>
                 </GridItem>
             </Grid>
 
@@ -51,6 +63,25 @@ const Building: FC<BuildingProps> = ({building}) => {
                 </Badge>
             </Box>
         </Box>
+    )
+}
+
+
+interface BuildingImageGridItemProps {
+    position: number
+    buildingImages: BuildingImageRetrieve[]
+}
+
+const BuildingImageGridItem: FC<BuildingImageGridItemProps> = ({buildingImages, position}: BuildingImageGridItemProps) => {
+    let path = "/empty-img.png" // by default each image is empty
+    if (buildingImages.length >= position)
+        if (position === 1)
+            path = buildingImages[position].image.medium
+        else 
+            path = buildingImages[position].image.small
+
+    return (
+        <Image src={path} layout="fill" objectFit="cover" alt="Building"/>
     )
 }
 
--- utils/data.ts
+++ utils/data.ts
@@ -1,8 +1,8 @@
-import { BuildingList, BuildingRetrieve } from "api_clients"
+import { BuildingList, BuildingRetrieve, OpenAPI } from "api_clients"
 
 
 export const getFirstImage = (building: BuildingList|BuildingRetrieve): string => {
     if (building.building_images.length > 0)
-        return building.building_images[0].image.small
+        return OpenAPI.BASE + building.building_images[0].image.medium
     return "https://picsum.photos/300/100"
 }
