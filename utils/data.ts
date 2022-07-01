6c6822a30dc4931d97a38e485dcfc7651932c77b
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-07-01 00:02:12 -0500

    :art: set /empty-img.png on marker infowindow if there arent any building image

--- utils/data.ts
+++ utils/data.ts
@@ -4,5 +4,5 @@ import { BuildingList, BuildingRetrieve, OpenAPI } from "api_clients"
 export const getFirstImage = (building: BuildingList|BuildingRetrieve): string => {
     if (building.building_images.length > 0)
         return OpenAPI.BASE + building.building_images[0].image.medium
-    return "https://picsum.photos/300/100"
+    return "/empty-img.png"
 }
