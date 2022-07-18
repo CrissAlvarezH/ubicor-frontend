b7209493b3cf770c4b0e4174d75e9a4b44fb5b2c
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-07-17 19:31:01 -0500

    :construction: WIP add input to select location on google map on create building form

--- components/SimpleMap.tsx
+++ components/SimpleMap.tsx
@@ -1,5 +1,5 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import { FC } from "react";
+import { GoogleMap, LoadScript, Marker, GoogleMapProps } from "@react-google-maps/api";
+import { FC, useState } from "react";
 
 
 const containerStyle = {
@@ -23,41 +23,48 @@ const defaultMapOptions: google.maps.MapOptions = {
 }
 
 
-interface SimpleMapProps {
+type SimpleMapProps = GoogleMapProps & {
     zoom?: number
     markers?: google.maps.LatLngLiteral[]
     center?: google.maps.LatLngLiteral
     options?: google.maps.MapOptions
 }
 
-const SimpleMap: FC<SimpleMapProps> = ({
-    zoom = 13, markers = [], center = {lat: 8.4116493, lng: -75.5885057},
-    options = {}
+export const SimpleMap: FC<SimpleMapProps> = ({
+    zoom = 16, markers = [], center = {lat: 8.4116493, lng: -75.5885057},
+    options = {}, ...props
 }) => {
     const mapOptions = {...defaultMapOptions, ...options}
 
     return (
-        <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY || ""}>
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                options={mapOptions}
-                zoom={zoom}
-                center={center}
-                onClick={() => console.log("on map click")}
-                onCenterChanged={() => console.log("center", center)}
-            >
+        <GoogleMap
+            {...props}
+            mapContainerStyle={containerStyle}
+            options={mapOptions}
+            zoom={zoom}
+            center={center}
+            onClick={() => console.log("map click")}
+        >
 
-                {
-                    markers.map((m: google.maps.LatLngLiteral, i: number) => (
-                        <Marker key={i} position={m}/>
-                    ))
-                }
+            {
+                markers.map((m: google.maps.LatLngLiteral, i: number) => (
+                    <Marker key={i} position={m}/>
+                ))
+            }
 
-            </GoogleMap>
-        </LoadScript>
+        </GoogleMap>
     )
 }
 
 
+interface MapWrapperProp {
+    children?: any
+}
 
-export default SimpleMap
+export const MapWrapper: FC<MapWrapperProp> = ({children}: MapWrapperProp) => {
+    return (
+        <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY || ""}>
+            {children}
+        </LoadScript>
+    )
+}
--- pages/[university_slug]/create-building.tsx
+++ pages/[university_slug]/create-building.tsx
@@ -1,82 +1,125 @@
-import { Box, Button, HStack, Modal, ModalContent, ModalOverlay, Text, useDisclosure, VStack } from "@chakra-ui/react"
+import { Box, Button, HStack, IconButton, Modal, ModalContent, ModalOverlay, StackDivider, Text, useDisclosure, VStack } from "@chakra-ui/react"
 import BackNavBar from "components/BackNavBar"
 import InputField from "components/InputField"
 import { Form, Formik } from "formik"
 import { useRouter } from "next/router"
-import { FC, useRef, useState } from "react"
-import BuildingMap from "components/BuildingMap"
-import SimpleMap from "components/SimpleMap"
+import { FC, useEffect, useRef, useState } from "react"
+import { SimpleMap, MapWrapper } from "components/SimpleMap"
+import { AddIcon } from "@chakra-ui/icons"
 
 
 
 const CreateBuildingPage = () => {
     const router = useRouter()
     const {isOpen: isSetPositionDialogOpen, onToggle: onToggleSetPositionDialog} = useDisclosure()
+
     
     return (
-        <>
+        <MapWrapper>
             <BackNavBar title={`Crear bloque para ${router.query.university_slug}`}/>    
 
-            <MapSetPositionDialog isOpen={isSetPositionDialogOpen} onClose={onToggleSetPositionDialog}
-                onSetPosition={(position) => console.log(position)}/>
+            <Box display="flex" justifyContent="center">
+                <VStack align="stretch" flex={1} maxW="600px" pt={{"base": 5, "md": 10}} px={5}>
+                    <Formik
+                        initialValues={{name: "", code: "", position: undefined}}
+                        onSubmit={(data) => console.log(data)}>
+                        {({isSubmitting, values, setFieldValue}) => (
+                            <Form>
+                                <MapSetPositionDialog 
+                                    mapCenter={values.position}
+                                    isOpen={isSetPositionDialogOpen} onClose={onToggleSetPositionDialog}
+                                    onSetPosition={(position) => setFieldValue("position", position)}/>
 
-            <VStack align="stretch" pt={{"base": 5, "md": 10}} px={5}>
-                <Formik
-                    initialValues={{name: "", code: "", position: undefined}}
-                    onSubmit={(data) => console.log(data)}>
-                    {({isSubmitting, values, handleChange}) => (
-                        <Form>
-                            <InputField
-                                name="name"
-                                placeholder="nombre"
-                                label="Nombre"/>
-
-                            <Box pt={3}>
                                 <InputField
-                                    name="code"
-                                    placeholder="codigo"
-                                    label="Codigo"/>
-                            </Box>
+                                    name="name"
+                                    placeholder="nombre"
+                                    label="Nombre"/>
 
-                            <Box borderColor="gray.200" rounded="md" mt={7} borderWidth={1.5}>
-                                <HStack justify="space-between" pl={3}>
-                                    <Text fontWeight="semibold">Ubicación</Text>
-                                    <Button 
-                                        onClick={() => onToggleSetPositionDialog()}
-                                        colorScheme="blue" variant="ghost">Editar</Button>
-                                </HStack>
-                                <Box h={52}>
-                                    <SimpleMap />
-
-                                    {/* <SimpleMap 
-                                        center={{lat: 8.4116493, lng: -75.5885057}}
-                                        markers={[{lat: 8.4116493, lng: -75.5885057}]}/> */}
+                                <Box pt={3}>
+                                    <InputField
+                                        name="code"
+                                        placeholder="codigo"
+                                        label="Codigo"/>
                                 </Box>
-                            </Box>
 
-                            <Button 
-                                w="100%" mt={10} type="submit"
-                                isLoading={isSubmitting} colorScheme="teal">
-                                Crear bloque
-                            </Button>
-                        </Form>
-                    )}
-                </Formik>
-            </VStack>
-        </>
+                                {/* Zones */}
+                                <Box mt={3}>
+                                    <Text pb={2} fontWeight="semibold">Zona</Text>
+                                    <HStack 
+                                        divider={<StackDivider/>} spacing={0}
+                                        borderWidth={1.5} rounded="md" borderColor="gray.200">
+                                        <HStack flex={1}>
+
+                                        </HStack>
+                                        <IconButton variant="ghost" aria-label="Agregar zona" icon={<AddIcon />}/>
+                                    </HStack>
+                                </Box>
+
+                                {/* Location */}
+                                <Box borderColor="gray.200" rounded="md" mt={7} borderWidth={1.5}>
+                                    <HStack justify="space-between" pl={3}>
+                                        <Text fontWeight="semibold">Ubicación</Text>
+                                        <Button 
+                                            onClick={() => onToggleSetPositionDialog()}
+                                            colorScheme="blue" variant="ghost">
+                                            { values.position ? "Editar" : "Agregar"}
+                                        </Button>
+                                    </HStack>
+                                        
+                                    {
+                                        values.position ? (
+                                            <Box h={52}>
+                                                <SimpleMap 
+                                                    center={values.position}
+                                                    markers={[values.position]}
+                                                    options={{gestureHandling: "none"}}/>
+                                            </Box>
+                                        ) : (
+                                            <Text 
+                                                w="100%" textAlign="center" p={10}
+                                                color="gray" fontSize="sm">
+                                                No establecida
+                                            </Text>
+                                        )
+                                    }
+                                </Box>
+
+                                <Button 
+                                    w="100%" mt={10} type="submit"
+                                    isLoading={isSubmitting} colorScheme="teal">
+                                    Crear bloque
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
+                </VStack>
+            </Box>
+        </MapWrapper>
     )
 }
 
 interface MapSetPositionDialogProps {
+    mapCenter: google.maps.LatLngLiteral | undefined
     isOpen: boolean
     onClose: () => void
-    onSetPosition: (position: google.maps.LatLng | undefined) => void
+    onSetPosition: (position: google.maps.LatLngLiteral | undefined) => void
 }
 
-const MapSetPositionDialog: FC<MapSetPositionDialogProps> = ({isOpen, onClose, onSetPosition}: MapSetPositionDialogProps) => {
+const MapSetPositionDialog: FC<MapSetPositionDialogProps> = ({mapCenter, isOpen, onClose, onSetPosition}: MapSetPositionDialogProps) => {
     const cancelRef = useRef<any>()
 
-    const [position, setPosition] = useState<google.maps.LatLng>()
+    const [map, setMap] = useState<google.maps.Map>()
+
+    const handleOnSetPosition = () => {
+        const center = map?.getCenter()
+        if (center) {
+            const position = {
+                lat: center.lat(), lng: center.lng()
+            }
+            onSetPosition(position)
+            onClose()
+        }
+    }
 
     return (
         <Modal
@@ -89,15 +132,21 @@ const MapSetPositionDialog: FC<MapSetPositionDialogProps> = ({isOpen, onClose, o
                 <Box h="100%" position="relative">
                     <Box display="flex" justifyContent="center" alignItems="center"
                         position="absolute" top={0} bottom={0} left={0} right={0}>
-                        <Box zIndex={10} p={2} bg="black"></Box>
+                        <Box 
+                            bg="blackAlpha.300"
+                            p={2.5} rounded="full" borderWidth={2} borderColor="black" zIndex={10}>
+                            <Box p={0.5} bg="black" rounded="full"/>
+                        </Box>
                     </Box>
-                    <SimpleMap />
+                    <SimpleMap 
+                        center={mapCenter}
+                        onLoad={(map) => setMap(map)}/>
                 </Box>
                 <HStack justify="end">
                     <Button colorScheme="gray" variant="ghost" ref={cancelRef} onClick={onClose}>
                         Cancelar
                     </Button>
-                    <Button colorScheme="green" variant="ghost" onClick={() => onSetPosition(position)} ml={3}>
+                    <Button colorScheme="green" variant="ghost" onClick={handleOnSetPosition}>
                         Guardar
                     </Button>
                 </HStack>
