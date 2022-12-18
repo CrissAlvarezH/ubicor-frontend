7516a933c57a20f57b56e0b964f04687498fa36e
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-12-18 11:44:46 -0500

    :goal_net: catch login errors

--- pages/profile/login.tsx
+++ pages/profile/login.tsx
@@ -3,7 +3,7 @@ import BackNavBar from "components/BackNavBar"
 import { GitHubIcon, GoogleIcon } from "components/Icons"
 import InputField from "components/InputField"
 import { Form, Formik } from "formik"
-import { GetServerSideProps } from "next"
+import { GetServerSideProps, NextPage } from "next"
 import { unstable_getServerSession } from "next-auth"
 import { signIn, SignInOptions } from "next-auth/react"
 import { authOptions } from "pages/api/auth/[...nextauth]"
@@ -16,7 +16,11 @@ const LoginFormSchema = Yup.object().shape({
 })
 
 
-const LoginPage = () => {
+interface LoginPageProps {
+    error?: string
+}
+
+const LoginPage: NextPage<LoginPageProps> = ({error}) => {
     const handleOnLogin = async (provider: string, data?: SignInOptions) => {
         await signIn(provider, data)
     }
@@ -26,6 +30,12 @@ const LoginPage = () => {
             <BackNavBar title="Login"/>
             <Box display="flex" justifyContent="center">
                 <VStack align="stretch" flex={1} maxW="600px" pt={{"base": 5, "md": 10}} pb={10} px={5}>
+                    {
+                        error && <Box py={2} px={2} mb={3} bg="orange.100" rounded="lg" color="orange.600">
+                            <Text fontSize="sm">Credenciales o contraseña incorrectas</Text>
+                        </Box>
+                    }
+
                     <Formik
                         initialValues={{email: "", password: ""}}
                         validationSchema={LoginFormSchema}
@@ -81,8 +91,9 @@ const LoginPage = () => {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const session = await unstable_getServerSession(ctx.req, ctx.res, authOptions);
     const {p = '/', error} = ctx.query;
+    console.log({error})
 
-    if ( session ) {
+    if (session) {
         return {
             redirect: {
                 destination: p.toString(),
@@ -92,7 +103,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     }
 
     return {
-        props: {}
+        props: {
+            error: error || null
+        }
     }
 }
 
