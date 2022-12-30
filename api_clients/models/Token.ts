8183b14b466dfd64a7e358f525144f3cb0adbbb3
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-12-30 18:19:43 -0500

    :alien: update models to consume api

--- api_clients/models/Token.ts
+++ api_clients/models/Token.ts
@@ -2,10 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import type { UserRetrieve } from './UserRetrieve';
-
 export type Token = {
-    user: UserRetrieve;
     access_token: string;
     token_type?: string;
 };
