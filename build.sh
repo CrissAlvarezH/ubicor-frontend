4cfdf0cda16474bf96205a077ba3aada61751c8c
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-12-31 14:04:35 -0500

    :hammer: add version to build.sh script

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+version=$1
+[ -z $version ] && version=0.1 && echo "\nset defaul version 0.1\n"
 
-docker build --build-arg BUILD_TIME_API_BASE_URL=http://api.ubicor.alvarezcristian.com --no-cache -t crissalvarezh/ubicor-frontend:0.1 .
+docker build --build-arg BUILD_TIME_API_BASE_URL=http://api.ubicor.alvarezcristian.com --no-cache -t crissalvarezh/ubicor-frontend:$version .
 
-docker tag crissalvarezh/ubicor-frontend:0.1 crissalvarezh/ubicor-frontend:latest
+docker tag crissalvarezh/ubicor-frontend:$version crissalvarezh/ubicor-frontend:latest
 
-docker push crissalvarezh/ubicor-frontend:0.1
+docker push crissalvarezh/ubicor-frontend:$version
 docker push crissalvarezh/ubicor-frontend:latest
