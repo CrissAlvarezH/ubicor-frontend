8349ad40da1b7ed4938e3433ceae73f5b8bbe038
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-01-02 00:37:29 -0500

    :children_crossing: create revalidate endpoint to revalida page data fron universities

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,40 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { BuildingsService } from "api_clients";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // if (!req.query.secret || req.query.secret != process.env.REVALIDATE_PAGE_SECRET) 
-    //     return res.status(401).json({error: "Invalid token"})
+    if (!req.query.secret || req.query.secret != process.env.REVALIDATE_PAGE_SECRET) 
+        return res.status(401).json({error: "Invalid token"})
+    
+    req.headers.authorization
 
-    if (!req.query.path)
-        return res.status(400).json({error: "'path' is required"})
-    if (!req.query.universities)
-        return res.status(400).json({error: "'universities' is required"})
+    if (!req.query.university)
+        return res.status(400).json({error: "'university' is required"})
     if (!req.query.buildings)
         return res.status(400).json({error: "'buildings' is required"})
-    if (req.query.universities.includes(",") && req.query.buildings != "__all__")
-        return res.status(400).json({error: "for several universities, buildings must be '__all__'"})
 
-    const universities = (req.query.universities.includes(",") 
-                          ? req.query.universities.toString().split(",") 
-                          : [req.query.universities.toString()])
+    const university = req.query.university.toString()
+
+    let buildings = []
+    if (req.query.buildings == "__all__") {
+        // fetch all building of thi university
+        const buildingResponse = await BuildingsService.buildingsList(university)
+        buildings = buildingResponse.map(b => b.code)
+    } else {
+        buildings = (
+            req.query.buildings.includes(",") 
+            ? req.query.buildings.toString().split(",") 
+            : [req.query.buildings.toString()]
+        )
+    }
 
     try {
-        for (const university of universities) {
-            let buildings: Array<string> = []
-            if (req.query.buildings == "__all__") {
-                // TODO fetch university buildings
-                buildings = []
-            } else {
-                buildings = (req.query.buildings.includes(",") 
-                     ? req.query.buildings.toString().split(",") 
-                     : [req.query.buildings.toString()])
-            }
+        // revalidate university index page
+        res.unstable_revalidate(`/${university}/map`)
+        res.unstable_revalidate(`/${university}/buildings`)
 
-            for (const building of buildings) {
-                // TODO revalidate buildings routes
-            }
+        for (const building of buildings) {
+            res.unstable_revalidate(`/${university}/buildings/${building}`)
         }
-        await res.unstable_revalidate(req.query.path.toString())
+
         return res.json({finish: true})
     } catch (error) {
         console.log("ERROR", error)
